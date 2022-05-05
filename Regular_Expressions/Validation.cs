02275e0c6e27746ab0ae36beda2b7992aaaedc63
maheshpapolu <maheshnaidu9666@gmail.com>
2022-05-05 23:52:06 +0530

    [Mahesh]Check_Valid_First_Name

---
+++ Regular_Expressions/Validation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace RegularExpressionAssignment
+{
+    internal class Validation
+    {
+        public static bool FirstNameValidation(string fName) // Creating a method for First name validation
+        {
+            string pattern = "^[A-Z][a-z]{2,}$"; // Regex for first name validation
+            //If name entered by user is match with regex then it is valid otherwise not
+            if (Regex.IsMatch(fName, pattern))
+            {
+                Console.WriteLine($"\nYour first name \"{fName}\" is valid");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"\nYour first name \"{fName}\" is not valid");
+                Console.WriteLine("\nPlease follow naming convention of first name");
+                return false;
+            }
+        }
+    }
+}
